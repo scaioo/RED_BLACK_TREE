8d6da374f44e94a4393e25e66f070ffc004a3dbd
scaioo <andrea.scaioli@gmail.com>
2026-02-08 18:47:44 +0100

    start coding RedBlackTree.cpp

--- src/RedBlackTree.cpp
+++ src/RedBlackTree.cpp
@@ -1,1 +1,19 @@
-#include <stdio.h>
+#ifndef RedBlackTree_h
+#define RedBlackTree_h
+class RedBlackTree {
+    private:
+        // Define the color type
+        enum Color {RED, BLACK};
+
+        // Define the structure of a node in the red-black tree
+        struct Node {
+            int data;
+            Color color;
+            Node *right, *left, *parent;
+
+            Node (int val) : data(val), color(RED) , left(nullptr), right(nullptr), parent(nullptr) {};    
+        };
+
+};
+
+#endif
